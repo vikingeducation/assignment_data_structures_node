dfc76ebe9a388b01a811a06a8b3f87c873398c3f
Eric Glover <etgt28@mail.missouri.edu>
2017-09-20 15:45:34 -0500

    queueing

--- stack.js
+++ stack.js
@@ -1,15 +1,17 @@
 const Stack = class {
   constructor() {
     this.storage = [];
+    this.length = 0;
   }
   pop() {
-    return;
+    return this.storage.splice(--this.length, 1)[0];
   }
-  push() {
-    return;
+  push(data) {
+    this.storage[this.length] = data;
+    return ++this.length;
   }
   peek() {
-    return;
+    return this.storage[this.length - 1];
   }
   isEmpty() {
     if (this.storage.length === 0) return true;
@@ -17,24 +19,15 @@ const Stack = class {
   }
 };
 
-// const Stack = () => {
-//   const storage = [];
-//   this.pop = () => {
-//     return;
-//   };
-//   this.push = () => {
-//     return;
-//   };
-//   this.peek = () => {
-//     return;
-//   };
-//   this.isEmpty = () => {
-//     if (storage.length === 0) return true;
-//     return false;
-//   };
-// };
-
+console.log("testing stack");
 const ourStack = new Stack();
 
-console.log("testing stack");
-console.log("stack empty?", ourStack.isEmpty());
+console.log("stack empty?, ", ourStack.isEmpty());
+console.log("pushing 1, ", ourStack.push(1));
+console.log("popping, ", ourStack.pop());
+
+console.log("pushing 3, ", ourStack.push(3));
+console.log("pushing 7, ", ourStack.push(7));
+console.log("pushing 4, ", ourStack.push(4));
+console.log("peeking should be 4, ", ourStack.peek());
+console.log("stack empty?, ", ourStack.isEmpty());
