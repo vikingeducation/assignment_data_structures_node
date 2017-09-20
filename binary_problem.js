e8680a3a193efb90d0a7ebd326a411debc871be7
Greg Filipczak <gregfilipczak@gmail.com>
2017-09-20 22:21:41 +0000

    getting closer

--- binary_problem.js
+++ binary_problem.js
@@ -3,12 +3,36 @@
 // cut in half for each, if each half's sum is equal => print
 // disregard the middle number
 
-const binPrinter = n => {
-  let arr = [];
-  for (let i = 2 ** n - 1; i >= 2 ** (n - 1); i--) {
-    arr.push(i.toString(2));
+// const binPrinter = n => {
+//   let arr = [];
+//   for (let i = 2 ** n - 1; i >= 2 ** (n - 1); i--) {
+//     arr.push(i.toString(2));
+//   }
+//   console.log(arr);
+// };
+//
+// binPrinter(4);
+
+// while val < 2**n - 1 && val > 2**(n-1)
+//   doRecursiveCheck(val-1).toString(2)
+let result = [];
+const binaryChecker = (n, val) => {
+  let value = 2 ** n - val;
+
+  let baseCase = 2 ** (n - 1);
+
+  if (value === baseCase) return result;
+  let binValue = value.toString(2).split("");
+  if (binValue % 2 !== 0) {
+    binValue.splice(binValue.length / 2, 1);
   }
-  console.log(arr);
+
+  let firstHalf = binValue.slice(0, Math.floor(binValue.length / 2));
+  let secondHalf = binValue.slice(
+    Math.floor(binValue.length / 2),
+    binValue.length
+  );
+  return binaryChecker(n, val + 1);
 };
 
-binPrinter(6);
+console.log(binaryChecker(5, 1));
