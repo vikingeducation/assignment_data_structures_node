92149e3d806b769414c4bb05653f5a021bc52837
Will Timpson <will.timpson@gmail.com>
2017-09-20 15:47:56 -0400

    naive stack implementation

--- stack.js
+++ stack.js
@@ -2,9 +2,25 @@ class Stack {
   constructor() {
     this.stack = [];
   }
-  push() {}
-  pop() {}
-  empty() {}
+  push(n) {
+    let newStack = [n];
+    for (let i = 0; i < this.stack.length; i++) {
+      newStack[i + 1] = this.stack[i];
+    }
+    this.stack = newStack;
+  }
+  pop() {
+    const popped = this.stack[0];
+    let newStack = [];
+    for (let i = 1; i < this.stack.length; i++) {
+      newStack[i - 1] = this.stack[i];
+    }
+    this.stack = newStack;
+    return popped;
+  }
+  empty() {
+    return !this.stack.length;
+  }
 }
 
 module.exports = Stack;
