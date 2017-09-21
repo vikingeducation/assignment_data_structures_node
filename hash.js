27c7f452b292336efb7c0063010a2ed552f02325
Andrew Senner <me@andrewsenner.com>
2017-09-21 12:50:37 -0400

    insert define methods working:

--- hash.js
+++ hash.js
@@ -1,12 +1,31 @@
-class Hash {
-  constructor(buckets = new Array(26)) {
-    this.buckets = buckets;
-  }
+const LinkedList = require('./linkedList');
 
-  hash(word) {
-    const charcode = word.charCodeAt(0);
-    return charcode < 91 ? charcode - 65 : charcode - 97;
-  }
+class Hash {
+	constructor(buckets = new Array(26)) {
+		this.buckets = buckets;
+	}
+
+	hash(word) {
+		const charcode = word.charCodeAt(0);
+		return charcode < 91 ? charcode - 65 : charcode - 97;
+	}
+
+	insert(word, definition) {
+		const wordHash = this.hash(word);
+		if (!this.buckets[wordHash]) {
+			this.buckets[wordHash] = new LinkedList();
+		}
+		this.buckets[wordHash].add(word, definition);
+	}
+
+	define(word) {
+		const wordHash = this.hash(word);
+		let node = this.buckets[wordHash].head;
+		while (node.next && node.word !== word) {
+			node = node.next;
+		}
+		return node.word === word ? node.definition : null;
+	}
 }
 
 module.exports = Hash;
--- spec/hashSpec.js
+++ spec/hashSpec.js
@@ -1,15 +1,37 @@
-const Hash = require("../hash");
+const Hash = require('../hash');
 
-describe("The Hash class", () => {
-  let hash;
-  const buckets = new Array(26);
-  beforeEach(() => {
-    hash = new Hash(buckets);
-  });
+describe('The Hash class', () => {
+	let hash, buckets;
+	beforeEach(() => {
+		buckets = new Array(26);
+		hash = new Hash(buckets);
+	});
 
-  it("has a hash method that returns an array index", () => {
-    expect(hash.hash("banana")).toEqual(1);
-    expect(hash.hash("Potato")).toEqual(15);
-    expect(hash.hash("zebra")).toEqual(25);
-  });
+	it('has a hash method that returns an array index', () => {
+		expect(hash.hash('banana')).toEqual(1);
+		expect(hash.hash('Potato')).toEqual(15);
+		expect(hash.hash('zebra')).toEqual(25);
+	});
+
+	it('has an insert method that inserts the value into the correct bucket', () => {
+		hash.insert('foo', 'bar');
+		hash.insert('frog', 'an Ape');
+		hash.insert('zooanimal', 'some animal in a zoo dude');
+		expect(buckets[5].tail.word).toEqual('foo');
+		expect(buckets[5].tail.definition).toEqual('bar');
+		expect(buckets[5].head.word).toEqual('frog');
+		expect(buckets[5].head.definition).toEqual('an Ape');
+		expect(buckets[25].head.word).toEqual('zooanimal');
+		expect(buckets[25].head.definition).toEqual('some animal in a zoo dude');
+	});
+
+	it('has a define method that takes a word and locates its definition', () => {
+		hash.insert('apple', 'bar');
+		hash.insert('frog', 'an Ape');
+		hash.insert('zooanimal', 'some animal in a zoo dude');
+
+		expect(hash.define('apple')).toEqual('bar');
+		expect(hash.define('frog')).toEqual('an Ape');
+		expect(hash.define('zooanimal')).toEqual('some animal in a zoo dude');
+	});
 });
