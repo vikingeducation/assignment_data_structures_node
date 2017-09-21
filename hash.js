667ae465928571a83a81ca5d0b2e84d960d73bbe
Andrew Senner <me@andrewsenner.com>
2017-09-21 17:23:52 -0400

    int hashing bruh

--- hash.js
+++ hash.js
@@ -1,74 +1,81 @@
-const LinkedList = require("./linkedList");
+const LinkedList = require('./linkedList');
+
+const PRIME = 1000000000003;
+const INT_PRIME = 12659874552859;
+const SEED = 12659874552859;
+const INT_SEED_A = 5969945408034;
+const INT_SEED_B = 9105117167006;
 
 class Hash {
-  constructor(entries) {
-    this.numBuckets = 30000;
-    this.buckets = new Array(30000);
-    this.prime = 1000000000003;
-    this.seed = 199079922168;
-    if (entries) {
-      entries.forEach(([word, definition]) => {
-        this.insert(word, definition);
-      });
-    }
-  }
+	constructor(entries) {
+		this.numBuckets = 26;
+		this.buckets = new Array(this.numBuckets);
+		if (entries) {
+			entries.forEach(([word, definition]) => {
+				this.insert(word, definition);
+			});
+		}
+	}
 
-  hash(word) {
-    const charSum = word.split("").reduce((sum, char, i) => {
-      return sum + char.charCodeAt(0) * this.seed ** i;
-    }, 0);
+	hash(word) {
+		const charSum = word.split('').reduce((sum, char, i) => {
+			return sum + char.charCodeAt(0) * SEED ** i;
+		}, 0);
 
-    return (charSum % this.prime) % this.numBuckets;
-  }
+		return (
+			INT_SEED_A * (charSum % PRIME) * INT_SEED_B % INT_PRIME % this.numBuckets
+		);
+	}
 
-  redistribute() {
-    console.log("Redistributing..............................................");
-    this.numBuckets *= 2;
-    this.buckets = this.buckets.reduce((newBuckets, bucket) => {
-      let node = bucket.head;
-      while (node) {
-        const wordHash = this.hash(node.word);
-        // console.log(wordHash);
-        if (!newBuckets[wordHash]) {
-          newBuckets[wordHash] = new LinkedList();
-        }
-        newBuckets[wordHash].add(node.word, node.definition);
-        node = node.next;
-      }
-      return newBuckets;
-    }, new Array(this.numBuckets));
-    console.log("Finished******************");
-  }
+	redistribute() {
+		console.log('Redistributing..............................................');
+		this.numBuckets *= 2;
+		this.buckets = this.buckets.reduce((newBuckets, bucket) => {
+			let node = bucket.head;
+			while (node) {
+				const wordHash = this.hash(node.word);
+				// console.log(wordHash);
+				if (!newBuckets[wordHash]) {
+					newBuckets[wordHash] = new LinkedList();
+				}
+				newBuckets[wordHash].add(node.word, node.definition);
+				node = node.next;
+			}
+			return newBuckets;
+		}, new Array(this.numBuckets));
+		console.log('Finished******************');
+	}
 
-  insert(word, definition) {
-    const wordHash = this.hash(word);
-    if (!this.buckets[wordHash]) {
-      this.buckets[wordHash] = new LinkedList();
-    }
-    this.buckets[wordHash].add(word, definition);
+	insert(word, definition) {
+		const wordHash = this.hash(word);
+		if (!this.buckets[wordHash]) {
+			this.buckets[wordHash] = new LinkedList();
+		}
+		this.buckets[wordHash].add(word, definition);
 
-    if (this.buckets[wordHash].length > 50) this.redistribute();
-  }
+		console.log(wordHash + ': ' + this.buckets[wordHash].length);
+		if (this.buckets[wordHash].length > 50) this.redistribute();
+	}
 
-  define(word, logger = () => null) {
-    let node = this.buckets[this.hash(word)].head;
-    let steps = 1;
+	define(word, logger = () => null) {
+		let node = this.buckets[this.hash(word)].head;
+		let steps = 1;
 
-    while (node.next && node.word !== word && ++steps) node = node.next;
-    logger(`Steps: ${steps}`);
-    return node.word === word ? node.definition : null;
-  }
+		while (node.next && node.word !== word && ++steps) node = node.next;
+		logger(`Steps: ${steps}`);
+		return node.word === word ? node.definition : null;
+	}
 
-  display(logger = console.log) {
-    let displayString = this.buckets.reduce((dString, bucket) => {
-      return dString + bucket;
-    }, "");
-    logger(displayString);
-  }
+	display(logger = console.log) {
+		let displayString = this.buckets.reduce((dString, bucket) => {
+			return dString + bucket;
+		}, '');
+		logger(displayString);
+	}
 
-  displayLength() {
-    return this.buckets.map((bucket, number) => `${number}: ${bucket.length}`);
-  }
+	displayLength() {
+		return this.buckets.map((bucket, number) => `${number}: ${bucket.length}`);
+	}
 }
 
 module.exports = Hash;
