e28078566dd6aeb28ef0ae755d79deb6b04620cc
EdwinYung <eyung7@gmail.com>
2018-01-17 17:34:58 -0500

    finished stacks and queues

--- stackAndQueue/StackAndQueue.js
+++ stackAndQueue/StackAndQueue.js
@@ -36,7 +36,7 @@ console.log(StackTest.empty(myArray));
 console.log(StackTest.empty(array2));
 
 const stackReverse = (string, arr = []) => {
-  let newStr = '';
+  let newStr = "";
   for (let i = 0; i < string.length; i++) {
     StackTest.push(arr, string[i]);
   }
@@ -46,8 +46,63 @@ const stackReverse = (string, arr = []) => {
   return newStr;
 };
 
-console.log(stackReverse('Jeff&Edwin', []));
+console.log(stackReverse("Jeff&Edwin", []));
 
 class Queue {
   constructor() {}
+
+  enqueue(arr, val) {
+    arr[arr.length] = val;
+    return arr;
+  }
+
+  //shift
+  dequeue(arr) {
+    let firstElement = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i + 1];
+    }
+    arr.length -= 1;
+    return firstElement;
+  }
+  //check if empty
+  empty(arr) {
+    return arr.length === 0;
+  }
+
+  emptyQueue(arr) {
+    arr = [];
+    return arr;
+  }
 }
+
+//Tests
+let queueArr = [1, 2, 3, 4, 5, 6, 7];
+let emptyArr = [];
+
+const queueTest = new Queue();
+
+console.log(queueTest.enqueue(queueArr, 8));
+
+console.log(queueTest.dequeue(queueArr));
+console.log(queueArr);
+
+console.log(queueTest.empty(emptyArr));
+
+console.log(queueTest.emptyQueue(queueArr));
+
+//load and unload a string from queue
+
+let queueStr = (str, arr = []) => {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    queueTest.enqueue(arr, str[i]);
+  }
+  console.log(arr);
+  for (let i = 0; i < str.length; i++) {
+    newStr += queueTest.dequeue(arr);
+  }
+  return newStr;
+};
+
+console.log(queueStr("Iamahero"));
