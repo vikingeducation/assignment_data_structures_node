6002f5e068d62012f069388d37f5bc533c1dea32
anne <richardson.ae@gmail.com>
2018-02-28 10:52:14 -0600

    Implements js queue

--- queue.js
+++ queue.js
@@ -2,3 +2,38 @@
 // Add methods for empty
 // Verify that you can load and unload a string in the same order with your queue.
 
+class Queue {
+  constructor(){
+    this.values = ''
+  }
+
+  enqueue(item){
+    return this.values += item
+  }
+
+  dequeue(){
+    var firstChar = this.values[0]
+    var newString = ''
+
+    for(var i = 0; i < this.values.length -1; i++){
+      newString += this.values[i + 1]
+    }
+    this.values = newString
+    // console.log(firstChar);
+    return firstChar
+  }
+
+  peek(){
+    var output = this.values[0]
+    console.log(output);
+    return output
+  }
+}
+
+var myQueue = new Queue()
+myQueue.enqueue('c') // 'c'
+myQueue.enqueue('a') // 'ca'
+myQueue.enqueue('t') // 'cat'
+myQueue.peek() // 'c'
+myQueue.dequeue() // 'c'
+myQueue.peek() // 'a'
